6e5ddbde2602a89660d4748fe2f3a8e80119b1c4
teachmecs <41761143+teachmecs@users.noreply.github.com>
2018-08-10 08:45:08 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
 // tests go here; this will not be compiled when this package is used as a library
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    binary.displayBinaryLeds(500)
-    pause(__internal.__timePicker(1000))
-    binary.displayBinaryLeds(10)
-    pause(__internal.__timePicker(1000))
-
-})
